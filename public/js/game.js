d7de5b68523c85c273f31cfdd7d51fc1a70631d9
Abourass <abourassa@assetval.com>
2019-04-30 10:37:41 -0600

    Trying to create a dynamic magic map using sketch

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,19 @@
 let health = 1;
+let money = 0;
+let location = 'A Dirty Alley';
+
+// Map Script
+const magicMap = document.createElement('script');
+magicMap.type = 'text/javascript';
+let magicMapContents = `const rc = rough.canvas(document.getElementById('canvas'));
+//line and rectangle
+rc.rectangle(140, 10, 100, 100, {
+  fill: 'rgba(38, 38, 54, 0.97)',
+  fillStyle: 'solid',
+  roughness: 2
+});`;
+
+
 
 function introClick(number){
   health += number;
@@ -18,7 +33,15 @@ function introClick(number){
     document.getElementById('moneyUIDesc').classList.toggle('hidden');
     document.getElementById('locationUI').classList.toggle('hidden');
     document.getElementById('statPadding').classList.toggle('hidden');
+    document.getElementById('map').classList.toggle('hidden');
     document.getElementById('messageUI').innerHTML = "You sit up and try to remember what happened.. or to remember anything at all. What happened, who am I?!?";
+    try {
+      magicMap.appendChild(document.createTextNode(magicMapContents));
+      document.body.appendChild(magicMap);
+    } catch(err) {
+      magicMap.text = magicMapContents;
+      document.body.appendChild(magicMap)
+    }
   }
 }
 
