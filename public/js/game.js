1d9b1ba9f35507c2735df31ce07d590f282cfa25
Abourass <abourassa@assetval.com>
2019-04-30 10:12:05 -0600

    Simple Button

--- public/js/game.js
+++ public/js/game.js
@@ -14,6 +14,10 @@ function introClick(number){
     document.getElementById('messageUI').innerHTML = "You realize you're laying on cold concrete, in an alley of some sort. Your head swims..";
   } else if ( health === 15) {
     document.getElementById('introButton').classList.toggle('hidden');
+    document.getElementById('findingHomeButton').classList.toggle('hidden');
+    document.getElementById('moneyUIDesc').classList.toggle('hidden');
+    document.getElementById('locationUI').classList.toggle('hidden');
+    document.getElementById('statPadding').classList.toggle('hidden');
     document.getElementById('messageUI').innerHTML = "You sit up and try to remember what happened.. or to remember anything at all. What happened, who am I?!?";
   }
 }
