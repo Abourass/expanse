cead59531675a084c674177cd06dd094af0f57ca
Abourass <abourassa@assetval.com>
2019-05-06 14:18:35 -0600

    VUE

--- public/js/game2.mjs
+++ public/js/game2.mjs
@@ -2,7 +2,7 @@ import {User} from './userFunctions.mjs';
 import {nodeVisToggle, nodeContent} from './gameFunctions.mjs';
 
 // Initiate New User
-const player = new User();
+const player = new User('Antonio');
 
 var app = new Vue({
   el: '#app',
